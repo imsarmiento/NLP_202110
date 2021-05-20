775da3add7fff4747d27438fe465876e4ba85aa0
Allan Corinaldi <allan9899@gmail.com>
2021-05-20 15:14:15 -0500

    script de duplicados

---
+++ HPC/duplicates.py
@@ -0,0 +1,38 @@
+import json
+import pandas as pd
+from fuzzywuzzy import fuzz
+data = []
+
+inputFile = 'englishSmall.json'
+with open(inputFile, "r") as f:
+    for line in f:
+        data.append(json.loads(line))
+
+df = pd.json_normalize(data)
+arr = list(df['text'])
+to_delete = [] 
+dict_to_keep = {}
+percentage = 0
+comparisons = (len(arr)**2)/2
+cur_comps = 0
+prevEntero = 1
+for i in range(len(arr)):
+    text1 = arr[i]
+    dict_to_keep[i] = True
+    for j in range(i+1, len(arr)):
+        text2 = arr[j]
+        similarity = fuzz.token_sort_ratio(text1, text2)
+        if similarity > 90:
+            to_delete.append(j)
+        else:
+            dict_to_keep[j] = True
+        cur_comps += 1
+        percentage = (cur_comps)*100/comparisons
+    if int(percentage) % 5 < 1 and int(percentage) != prevEntero:
+        print(str(int(percentage)) + '%')
+        prevEntero = int(percentage)
+
+arr_keep = list(dict_to_keep.keys())
+print('Files to filter',len(to_delete))
+df_filtered = df.iloc[df.index.isin(arr_keep)]
+df_filtered.to_json('output1.json', orient='records')
