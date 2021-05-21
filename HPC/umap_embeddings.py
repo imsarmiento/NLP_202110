5557f4f34de94bb6ecccb070117e8babc6ad1c4c
Allan Corinaldi <allan9899@gmail.com>
2021-05-21 17:26:30 -0500

    conzolazos y cambio nombre de p1 a bert_embeddings

--- HPC/umap_embeddings.py
+++ HPC/umap_embeddings.py
@@ -10,12 +10,19 @@ if __name__ == "__main__":
     input_file = args[0]
     output_file = args[1]
 
+print('open bert_embeddings')
 with open(input_file, "rb") as file:
     embeddings = pk.load(file)
+print('close bert_embeddings')
 
+print('start training umap')
 umap_embeddings = umap.UMAP(n_neighbors=30,
                             n_components=5,
                             metric='cosine').fit_transform(embeddings)
+print('stop training umap')
 
+print('open umap_embeddings')
 with open(output_file, "wb") as umap_file:
     pk.dump(umap_embeddings, umap_file)
+print('close umap_embeddings')
+print('finalized')
